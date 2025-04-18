8a20d6c86867fb2c76f7f600d62fb690cc149057
bhargav-ultiweb <bargava.narasimhan@ultiweb.co.in>
2025-04-18 16:01:13 +0530

    Update Header to Intermediate

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App () {
 
 	return (
 		<>
-		  <h1> Advanced Git Training Project</h1>
+		  <h1>Intermediate Git Training Project</h1>
 		  <div>
 			<a href="https://vite.dev" target="_blank">
 			  <img src={viteLogo} className="logo" alt="Vite logo" />
