875b8cce60e3cc125ee643526919bca5d56cf1cf
bhargav2 <bargavashok@gmail.com>
2025-04-18 16:11:07 +0530

    Update Header

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App () {
 
 	return (
 		<>
-		  <h1>Intermediate Git Training Project</h1>
+		  <h1>Intermediate Git Training</h1>
 		  <div>
 			<a href="https://vite.dev" target="_blank">
 			  <img src={viteLogo} className="logo" alt="Vite logo" />
